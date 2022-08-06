1f9960b46c8abb788b02c5d8ad76ad4484613bdc
egan918 <lo.egan918@gmail.com>
2022-08-07 05:39:46 +0900

    contract number added

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
   email: "snowflake.dev918@gmail.com",
-  phone: "+353 61 748 222",
+  phone: "+1 678 999 8268",
   address: "3601 Flat Shoals Rd, Atlanta, GA, United states",
 
   sheetAPI: "",
